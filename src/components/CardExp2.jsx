c0d1ac29eb844416fe45f883aeb249a28538a2be
CodeImm <45065137+CodeImm@users.noreply.github.com>
2020-10-20 16:14:17 +0700

    card was changed

--- src/components/CardExp2.jsx
+++ src/components/CardExp2.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -7,14 +7,16 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Grid from "@material-ui/core/Grid";
+import Hidden from "@material-ui/core/Hidden";
 
 const useStyles = makeStyles({
-    root: {
+    card: {
         display: 'flex',
         flexDirection: 'row',
-        // maxWidth: 1000,
+        // maxWidth: 500
     },
-    details: {
+    cardDetails: {
         display: 'flex',
         // paddingLeft: '10px',
         // paddingTop: '10px',
@@ -23,8 +25,8 @@ const useStyles = makeStyles({
     content: {
         flex: '1 0 auto',
     },
-    cover: {
-        width: 300,
+    cardMedia: {
+        width: 150,
     },
     actions: {
         // display: 'flex',
@@ -35,45 +37,83 @@ const useStyles = makeStyles({
 
 export default function CardExp2(props) {
     const classes = useStyles();
-
     return (
-        <Card className={classes.root}>
-            {/*<CardActionArea>*/}
-
-                <CardMedia
-                    component="img"
-                    className={classes.cover}
-                    alt="Contemplative Reptile"
-                    height="140"
-                    image="https://source.unsplash.com/random"
-                    title="Contemplative Reptile"
-                />
-            <div className={classes.details}>
-                <CardContent  className={classes.content}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        {props.title}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        {props.description}
-                    </Typography>
-                </CardContent>
-
-            {/*</CardActionArea>*/}
-            <CardActions className={classes.actions}>
-                <Button size="small" color="primary">
-                    Theory
-                </Button>
-                <Button size="small" color="primary">
-                    Apparatus
-                </Button>
-                <Button size="small" color="primary">
-                    Test
-                </Button>
-                <Button size="small" color="primary">
-                    Run experiment
-                </Button>
-            </CardActions>
-            </div>
-        </Card>
+        <Grid item xs={12} md={6}>
+            <CardActionArea component="a" href="#">
+                <Card className={classes.card}>
+                    <div className={classes.cardDetails}>
+                        <CardContent>
+                            <Typography component="h2" variant="h5">
+                                {props.title}
+                            </Typography>
+                            <Typography variant="subtitle1" paragraph>
+                                {props.description}
+                            </Typography>
+                            <CardActions className={classes.actions}>
+                                <Button size="small" color="primary">
+                                    Theory
+                                </Button>
+                                <Button size="small" color="primary">
+                                    Apparatus
+                                </Button>
+                                <Button size="small" color="primary">
+                                    Run experiment
+                                </Button>
+                            </CardActions>
+                        </CardContent>
+                    </div>
+                    <Hidden xsDown>
+                        <CardMedia
+                            component="img"
+                            className={classes.cardMedia}
+                            alt="Contemplative Reptile"
+                            image="https://source.unsplash.com/random"
+                            title="Contemplative Reptile"
+                        />
+                        {/*// title={post.imageTitle} />*/}
+                    </Hidden>
+                </Card>
+            </CardActionArea>
+        </Grid>
+        // <Grid item xs={12} md={6}>
+        //     <Card className={classes.card}>
+        //         {/*<CardActionArea>*/}
+        //
+        //         <CardMedia
+        //             component="img"
+        //             className={classes.cover}
+        //             alt="Contemplative Reptile"
+        //             height="140"
+        //             image="https://source.unsplash.com/random"
+        //             title="Contemplative Reptile"
+        //         />
+        //         <div className={classes.cardDetails}>
+        //             <CardContent className={classes.content}>
+        //                 <Typography gutterBottom variant="h5" component="h2">
+        //                     {props.title}
+        //                 </Typography>
+        //                 <Typography variant="body2" color="textSecondary" component="p">
+        //                     {props.description}
+        //                 </Typography>
+        //             </CardContent>
+        //
+        //             {/*</CardActionArea>*/}
+        //             <CardActions className={classes.actions}>
+        //                 <Button size="small" color="primary">
+        //                     Theory
+        //                 </Button>
+        //                 <Button size="small" color="primary">
+        //                     Apparatus
+        //                 </Button>
+        //                 <Button size="small" color="primary">
+        //                     Test
+        //                 </Button>
+        //                 <Button size="small" color="primary">
+        //                     Run experiment
+        //                 </Button>
+        //             </CardActions>
+        //         </div>
+        //     </Card>
+        // </Grid>
     );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,35 +2,53 @@ import React from 'react';
 import CardExp2 from "../components/CardExp2";
 import Grid from "@material-ui/core/Grid";
 import FirebaseContext from "../firebase/context";
+import Container from "@material-ui/core/Container";
+import {makeStyles} from "@material-ui/core/styles";
 
 const cardsExp = [
     {
         title: "Лабораторная работа Определение скорости звука в воздухе",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci alias dignissimos eaque fuga fugit id maiores nulla placeat quam quasi tempora, velit veritatis, vero! Accusamus adipisci at eum provident voluptates."
+    },
+    {
+        title: "Лабораторная работа 2 Определение скорости звука в воздухе",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci alias dignissimos eaque fuga fugit id maiores nulla placeat quam quasi tempora, velit veritatis, vero! Accusamus adipisci at eum provident voluptates."
     }
 ]
 
+const useStyles = makeStyles((theme)=>({
+    mainGrid: {
+        display: 'flex',
+        flexGrow: 1,
+        paddingTop: theme.spacing(3)
+    }
+}));
+
 export default function HomePage() {
     const {user} = React.useContext(FirebaseContext);
+    const [spacing, setSpacing] = React.useState(2);
+    const classes = useStyles();
 
     const [cards] = cardsExp;
     return (
         <React.Fragment>
-                                {/*<MainFeaturedPost post={mainFeaturedPost} />*/}
-                    <Grid >
-                        {cardsExp.map((card) => (
-                            <CardExp2 key={card.title} title={card.title} description={card.description} />
-                        ))}
-                    </Grid>
-                    {/*<Grid container spacing={5} className={classes.mainGrid}>*/}
-                    {/*    <Main title="From the firehose" posts={posts} />*/}
-                    {/*    <Sidebar*/}
-                    {/*        title={sidebar.title}*/}
-                    {/*        description={sidebar.description}*/}
-                    {/*        archives={sidebar.archives}*/}
-                    {/*        social={sidebar.social}*/}
-                    {/*    />*/}
-                    {/*</Grid>*/}
+            <Container maxWidth="md" component="main">
+                <Grid container className={classes.mainGrid} spacing={spacing} justify="center">
+                    {/*{[0, 1, 2].map((value) => (*/}
+                    {cardsExp.map((card) => (
+                        <CardExp2 key={card.title} title={card.title} description={card.description}/>
+                    ))}
+                </Grid>
+            </Container>
+            {/*<Grid container spacing={5} className={classes.mainGrid}>*/}
+            {/*    <Main title="From the firehose" posts={posts} />*/}
+            {/*    <Sidebar*/}
+            {/*        title={sidebar.title}*/}
+            {/*        description={sidebar.description}*/}
+            {/*        archives={sidebar.archives}*/}
+            {/*        social={sidebar.social}*/}
+            {/*    />*/}
+            {/*</Grid>*/}
         </React.Fragment>
     );
 }
